6428fba3ca36aac4db20651d0618e6e896d5c894
KarRz <reza11781@gmail.com>
2025-05-17 00:00:41 +0700

    feat: add Exception for InputValidation and TestDriver

---
+++ src/main/exception/InvalidInputException.java
@@ -0,0 +1,35 @@
+package main.exception;
+
+public class InvalidInputException extends Exception {
+    /**
+     * Buat exception tanpa pesan khusus.
+     */
+    public InvalidInputException() {
+        super();
+    }
+
+    /**
+     * Buat exception dengan pesan kesalahan.
+     * @param message deskripsi kesalahan
+     */
+    public InvalidInputException(String message) {
+        super(message);
+    }
+
+    /**
+     * Buat exception dengan pesan dan penyebab.
+     * @param message deskripsi kesalahan
+     * @param cause penyebab asli (throwable)
+     */
+    public InvalidInputException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    /**
+     * Buat exception dengan penyebab saja.
+     * @param cause penyebab asli (throwable)
+     */
+    public InvalidInputException(Throwable cause) {
+        super(cause);
+    }
+}
